81fc9ad388926050092dcdea5af0e7040bfc4a45
Brian McPhillips <B.McPhillips89@gmail.com>
2021-07-24 23:06:48 -0700

    Get call to API

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 import request from 'superagent';
 
 export default class SearchPage extends Component {
-  
+  state ={
+    characterState: {}
+  }
+  componentDidMount = async() => {
+    const headers = {
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${process.env.ACCESS_TOKEN}` 
+    }
+    const data = await request.get('https://the-one-api.dev/v2/character', { headers: headers });
+
+    this.setState({ characterState: data });
+    console.log(this.state.characterState);
+
+  }
+
   render() {
     return (
       <div>

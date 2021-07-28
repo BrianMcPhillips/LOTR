8dc107efb68b0c029028687035cb1c3c5f7e6e96
Brian McPhillips <B.McPhillips89@gmail.com>
2021-07-28 16:23:19 -0700

    React-router-dom with detail page

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 import SearchPage from './SearchPage/SearchPage';
 import Header from './Header/Header';
+import DetailPage from './DetailPage/DetailPage';
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Router>
         <Header />
-        <SearchPage /> 
+          <Switch>
+            <Route
+              path='/'
+              exact
+              render={ (routerProps) => <SearchPage {...routerProps} /> }
+            />
+            <Route 
+              path='/detail/:characterId'
+              exact
+              render={ (routerProps) => <DetailPage {...routerProps} /> }
+            />
+          </Switch>
+        </Router>
+
       </div>
     )
   }

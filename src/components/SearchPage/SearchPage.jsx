02d6bb1b1ab28aab20864a5182e8ad13b815cd7d
Brian McPhillips <B.McPhillips89@gmail.com>
2021-07-29 01:53:15 -0700

    Pagination working

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -9,7 +9,9 @@ export default class SearchPage extends Component {
   state = {
     characterState: [],
     term: '',
-    searchBy: 'name'
+    searchBy: 'name',
+    currentPage: 1,
+    totalPage: 1
   }
 
   componentDidMount = async() => {
@@ -17,16 +19,25 @@ export default class SearchPage extends Component {
       .get('https://the-one-api.dev/v2/character?limit=20')
       .set('Authorization', 'Bearer ' + process.env.REACT_APP_ACCESS_TOKEN)
       .accept('application/json');
+    // console.log('initial---->', data);
 
-    this.setState({ characterState: data.body.docs });
+    this.setState({ 
+      characterState: data.body.docs,
+      totalPage: data.body.pages
+    });
   }
+
   makeRequest = async() => {
     const data = await request
-      .get(`https://the-one-api.dev/v2/character?${this.state.searchBy}=${this.state.term}&limit=20`)
+      .get(`https://the-one-api.dev/v2/character?${this.state.searchBy}=${this.state.term}&page=${this.state.currentPage}&limit=20`)
       .set('Authorization', 'Bearer ' + process.env.REACT_APP_ACCESS_TOKEN)
       .accept('application/json');
-    this.setState({ characterState: data.body.docs });
-    console.log(this.state.characterState);
+      // console.log('filter ====>', data);
+    this.setState({ 
+      characterState: data.body.docs,
+      totalPage: data.body.pages
+    });
+
   }
   handleSearchBy = (e) => {
     this.setState({ term: e.target.value})
@@ -37,9 +48,17 @@ export default class SearchPage extends Component {
   handleClick = (e) => {
     this.makeRequest()
   }
+  handleNext = () => {
+    this.setState({ currentPage: this.state.currentPage + 1 })
+    this.makeRequest()
+  }
+  handlePrev = () => {
+    this.setState({ currentPage: this.state.currentPage - 1 })
+    this.makeRequest()
+  }
 
   render() {
-    const { characterState } = this.state;
+    const { characterState, currentPage, totalPage } = this.state;
     return (
       <div className={styles.searchPage}>
         <SearchBar 
@@ -47,7 +66,12 @@ export default class SearchPage extends Component {
           hanOption={this.handleOption}
           options={options}
           hanClick={this.handleClick}/> 
-        <CharacterList data={characterState}/>
+        <CharacterList 
+          data={characterState}
+          next={this.handleNext}
+          prev={this.handlePrev}
+          page={currentPage}
+          totPages={totalPage}/>
       </div>
     )
   }
